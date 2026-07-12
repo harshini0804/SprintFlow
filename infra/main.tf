1c3f32b5cab5c9f6b28502f4eb1d60a90878f5ee
Harshini Gunturi <harshinigunturi08@gmail.com>
2026-07-12 21:24:59 +0530

    update EC2 DNS for lab session

--- infra/main.tf
+++ infra/main.tf
@@ -370,7 +370,7 @@ resource "aws_cloudfront_distribution" "frontend" {
 
   # Origin 2 — EC2 backend
   origin {
-    domain_name ="ec2-65-2-186-89.ap-south-1.compute.amazonaws.com"
+    domain_name ="ec2-13-234-120-195.ap-south-1.compute.amazonaws.com"
     origin_id   = "ec2-backend"
 
     custom_origin_config {
